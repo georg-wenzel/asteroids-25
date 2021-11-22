d9bb134878223a030de7675f4112a330751affd6
Florian Maier <1florian9@gmail.com>
2021-11-22 18:47:25 +0100

    Added MainMenu and Settings with basic functionality

---
+++ Asteroids99/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+
+    #region methods
+
+    public void PlaySinglePlayer()
+    {
+        // Load some Scene
+        // SceneManager.LoadScene("SomeSceneName");
+    }
+
+    public void PlayMultiPlayer()
+    {
+        // Load some Scene
+        // SceneManager.LoadScene("SomeSceneName");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+}
