4acd927ac307f0ed019dcbbde8d781a9ce68a85a
Georg Felix Wenzel <georg.wenzel@student.uibk.ac.at>
2021-11-25 15:55:20 +0100

    Add mock GameState for testing on other branches

---
+++ Asteroids99/Assets/Scripts/Game/GameState.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    #region properties
+    public bool GameOver { get; set; }
+    public int HP { get; set; }
+    public int Score { get; set; }
+    #endregion
+}
